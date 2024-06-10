135ee0d460dd4dec9deb7ccce4b076cd5291861b
MahyaAhmadzadeh <124697692+MahyaAhmadzadeh@users.noreply.github.com>
2024-06-10 11:10:49 -0400

    Create pytorch_env_setup.sh
    This script sets up a Conda environment for PyTorch with CUDA support and Keras.

---
+++ pytorch_env_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Create PyTorch environment
+conda create -n pytorch_env python=3.11.9 -y
+conda activate pytorch_env
+
+# Install PyTorch and Keras
+python -m pip install torch torchvision torchaudio keras
+
+# Set up activation and deactivation scripts
+cd $CONDA_PREFIX
+mkdir -p ./etc/conda/activate.d
+mkdir -p ./etc/conda/deactivate.d
+touch ./etc/conda/activate.d/env_vars.sh
+touch ./etc/conda/deactivate.d/env_vars.sh
+
+# Edit activate script
+echo '#!/bin/sh
+NVIDIA_DIR=$(dirname $(python -c "import nvidia;print(nvidia.__file__)"))
+export PREV_LDLIBPATH=$(echo ${LD_LIBRARY_PATH})
+export LD_LIBRARY_PATH=$(echo ${NVIDIA_DIR}/*/lib/ | sed -r "s/\s+/:/g")${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export KERAS_BACKEND="torch"' > ./etc/conda/activate.d/env_vars.sh
+
+# Edit deactivate script
+echo '#!/bin/sh
+export LD_LIBRARY_PATH=$(echo ${PREV_LDLIBPATH})' > ./etc/conda/deactivate.d/env_vars.sh
+
+# Verify installation
+conda deactivate
+conda activate pytorch_env
+echo $LD_LIBRARY_PATH
+python -c "import torch; print(torch.cuda.is_available());"
