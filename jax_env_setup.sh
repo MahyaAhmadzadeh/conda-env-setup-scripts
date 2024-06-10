502284a92afc39f632fc9eb52dcb0401f110ec44
MahyaAhmadzadeh <124697692+MahyaAhmadzadeh@users.noreply.github.com>
2024-06-10 11:10:08 -0400

    Create jax_env_setup.sh
    This script sets up a Conda environment for JAX with CUDA support and Keras.

---
+++ jax_env_setup.sh
@@ -0,0 +1,37 @@
+
+### `jax_env_setup.sh`
+```bash
+#!/bin/bash
+
+# Create JAX environment
+conda create -n jax_env python=3.11.9 -y
+conda activate jax_env
+
+# Install JAX and CUDA-enabled JAXLIB
+python -m pip install --upgrade pip
+python -m pip install jax[cuda12]
+python -m pip install keras
+
+# Set up activation and deactivation scripts
+cd $CONDA_PREFIX
+mkdir -p ./etc/conda/activate.d
+mkdir -p ./etc/conda/deactivate.d
+touch ./etc/conda/activate.d/env_vars.sh
+touch ./etc/conda/deactivate.d/env_vars.sh
+
+# Edit activate script (you alternatively can use any editor to paste the below in the scripts)
+echo '#!/bin/sh
+NVIDIA_DIR=$(dirname $(python -c "import nvidia;print(nvidia.__file__)"))
+export PREV_LDLIBPATH=$(echo ${LD_LIBRARY_PATH})
+export LD_LIBRARY_PATH=$(echo ${NVIDIA_DIR}/*/lib/ | sed -r "s/\s+/:/g")${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export KERAS_BACKEND="jax"' > ./etc/conda/activate.d/env_vars.sh
+
+# Edit deactivate script
+echo '#!/bin/sh
+export LD_LIBRARY_PATH=$(echo ${PREV_LDLIBPATH})' > ./etc/conda/deactivate.d/env_vars.sh
+
+# Verify installation
+conda deactivate
+conda activate jax_env
+echo $LD_LIBRARY_PATH
+python -c "import jax; print(jax.devices());"
